c908983fa42eeb507cbb9e926e11f19e05713417
diem <diemqnguyen67@gmail.com>
2024-07-20 16:03:50 -0500

    Worked on NewPlayerViewController

---
+++ DemonBuddy/DemonBuddy/NewNPCMonsterViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NewNPCMonsterViewController.swift
+//  DemonBuddy
+//
+//  Created by Diem-Quynh Nguyen on 7/20/24.
+//
+
+import UIKit
+
+class NewNPCMonsterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
