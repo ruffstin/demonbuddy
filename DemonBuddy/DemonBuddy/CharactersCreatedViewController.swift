f3807cc252501c4b442c974f0ab68e5e4d68d74a
Mark Mill <mlm7609vvd@gmail.com>
2024-07-23 19:03:54 -0500

    touch ups for removing excess screens beginning to work with core data and creating a pseudo character, currently getting a NSE exception upon trying to create a character.

--- DemonBuddy/DemonBuddy/CharactersCreatedViewController.swift
+++ DemonBuddy/DemonBuddy/CharactersCreatedViewController.swift
@@ -7,21 +7,98 @@
 
 import UIKit
 
-class CharactersCreatedViewController: UIViewController {
+import FirebaseAuth
+
+import CoreData
+
+let appDelegate = UIApplication.shared.delegate as! AppDelegate
+let context = appDelegate.persistentContainer.viewContext
+
+class CharactersCreatedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, CharacterCreationDelegate {
 
     @IBOutlet weak var tableView: UITableView!
     
+    var characterList: [NSManagedObject] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        fetchCharacter()
 
-        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    // allows us to fetch characters from core data
+    func fetchCharacter() {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+                let context = appDelegate.persistentContainer.viewContext
+                let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Character")
+                
+                // Filter characters by userID
+                if let userID = Auth.auth().currentUser?.uid {
+                    let predicate = NSPredicate(format: "userID == %@", userID)
+                    fetchRequest.predicate = predicate
+                }
+                
+                do {
+                    characterList = try context.fetch(fetchRequest)
+                    tableView.reloadData()
+                } catch {
+                    print("Failed to fetch characters: \(error)")
+                }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "createCharacter" {
+            if let destinationVC = segue.destination as? NewCharacterViewController {
+                destinationVC.delegate = self
+            }
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return characterList.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "charactercell", for: indexPath) as! CharacterViewTableCell
+        let character = characterList[indexPath.row]
+        // grab character and game name
+        cell.charName.text = (character.value(forKey: "name") as? String ?? "")
+        cell.gameName.text = (character.value(forKey: "gameName") as? String ?? "")
+        return cell
+    }
+
+    // Swipe to delete from tableView
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let characterToDelete = characterList[indexPath.row]
+            characterList.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            
+            context.delete(characterToDelete)
+            saveContext()
+        }
     }
     
     @IBAction func filterPressed(_ sender: Any) {
     }
     
-    
     @IBAction func addNotePressed(_ sender: Any) {
+        performSegue(withIdentifier: "createCharacter", sender: self)
     }
     
+}
+
+func saveContext () {
+    if context.hasChanges {
+        do {
+            try context.save()
+        } catch {
+            let nserror = error as NSError
+            fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+        }
+    }
 }
---
+++ DemonBuddy/DemonBuddy/NewCharacterViewController.swift
@@ -0,0 +1,57 @@
+//
+//  NewCharacterViewController.swift
+//  DemonBuddy
+//
+//  Created by Mark Mills on 7/23/24.
+//
+
+import UIKit
+
+import CoreData
+
+import FirebaseAuth
+
+protocol CharacterCreationDelegate: AnyObject {
+    func fetchCharacter()
+}
+
+class NewCharacterViewController: UIViewController {
+    
+    weak var delegate: CharacterCreationDelegate?
+    
+    
+    @IBOutlet weak var charGameName: UITextField!
+    
+    @IBOutlet weak var charName: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+
+    @IBAction func saveButtonPressed(_ sender: Any) {
+        // Save to Core Data
+        let character = NSEntityDescription.insertNewObject(forEntityName: "Character", into: context)
+        character.setValue(charName.text, forKey: "name")
+        character.setValue(charGameName.text, forKey: "gameName")
+        if let userID = Auth.auth().currentUser?.uid {
+                    character.setValue(userID, forKey: "userID")
+                }
+        
+        saveContext()
+        delegate?.fetchCharacter()
+    }
+    
+    func saveContext () {
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+    
+}
