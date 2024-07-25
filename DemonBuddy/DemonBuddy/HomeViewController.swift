3f4d4fd71f242bba5efc92f2e14548b1424a6a78
Joseph Arteaga <jarteaga999@icloud.com>
2024-07-25 14:05:29 -0500

    Placed Notes screen in vertical stack view and fixed notes button on Home page

--- DemonBuddy/DemonBuddy/HomeViewController.swift
+++ DemonBuddy/DemonBuddy/HomeViewController.swift
@@ -16,6 +16,8 @@ class HomeViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    
+    
 
     @IBAction func logoutPressed(_ sender: Any) {
         do {
