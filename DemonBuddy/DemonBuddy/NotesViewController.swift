900067e06e8512712d4b020cb58057c93f4e68d3
Joseph Arteaga <jarteaga999@icloud.com>
2024-07-26 18:10:29 -0500

    Finished filter/sort view for Notes view

--- DemonBuddy/DemonBuddy/NotesViewController.swift
+++ DemonBuddy/DemonBuddy/NotesViewController.swift
@@ -10,17 +10,45 @@ import UIKit
 class NotesViewController: UIViewController {
 
     
+    @IBOutlet weak var sortFiltersStackView: UIStackView!
+    @IBOutlet weak var filterButtonsStackView: UIStackView!
+    
+    @IBOutlet weak var filtersStackView: UIStackView!
+    @IBOutlet weak var selectFiltersView: UIView!
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        filtersStackView.addSubview(sortFiltersStackView)
+        filtersStackView.addSubview(filterButtonsStackView)
+        
+        selectFiltersView.addSubview(filtersStackView)
+        
+        view.addSubview(selectFiltersView)
+        selectFiltersView.layer.cornerRadius = 10
+        selectFiltersView.layer.masksToBounds = true
     }
     
     @IBAction func filterPressed(_ sender: Any) {
+        if selectFiltersView.isHidden {
+            selectFiltersView.isHidden = false
+        } else {
+            selectFiltersView.isHidden = true
+        }
     }
     
     @IBAction func addNotePressed(_ sender: Any) {
     }
+    
+    
+    @IBAction func filtersDonePressed(_ sender: Any) {
+        selectFiltersView.isHidden = true
+    }
+    
+    
+    @IBAction func filtersCancelPressed(_ sender: Any) {
+        selectFiltersView.isHidden = true
+    }
+    
 }
