0f59ddbdade3d7097c5a31e04ce339bd536afe08
Mark Mill <mlm7609vvd@gmail.com>
2024-08-07 09:21:21 -0500

    found the bug, need to tweak 1 line of code later :)

--- DemonBuddy/DemonBuddy/NewNPCMonsterViewController.swift
+++ DemonBuddy/DemonBuddy/NewNPCMonsterViewController.swift
@@ -132,106 +132,174 @@ class NewNPCMonsterViewController: UIViewController {
     
     // Create NPC or Monster
     @IBAction func savePressed(_ sender: Any) {
-        if ((titleTextView.text?.isEmpty) == nil) {
-            let controller = UIAlertController(
-                title: "Missing Title",
-                message: "Input a title",
-                preferredStyle: .alert
-            )
+            // Function to show alert
+            func showAlert(forMissingField fieldName: String) {
+                let controller = UIAlertController(
+                    title: "Missing \(fieldName)",
+                    message: "Please input a value for \(fieldName).",
+                    preferredStyle: .alert
+                )
+                
+                controller.addAction(UIAlertAction(title: "Ok", style: .default))
+                present(controller, animated: true)
+            }
             
-            controller.addAction(UIAlertAction(title: "Ok", style: .default))
-            present(controller, animated: true)
-        } else {
-        // add npc VS Monster?
-        let userID = Auth.auth().currentUser?.uid
-        let game = gameName.text // will be changed per Joseph's implementation?
-        let creatureName = creatureNameText.text
-        let maxHP = hitPoints.text
-        let armor = armorClass.text
-        let movement = speed.text
-        let cR = challengeRating.text
-        let xP = xpToEarn.text
-        let str = strength.text
-        let dex = dexterity.text
-        let con = constitution.text
-        let int = intelligence.text
-        let wis = intelligence.text
-        let cha = charisma.text
-        let skillsProf = skills.text
-        let radar = senses.text
-        let speech = languages.text
-        let abilities = creatureFeatures.text
-        let attacks = actions.text
-        let loot = items.text
-        
-        // npc vs monster?
-         
-        if monsterOrNpcToEdit != nil{
-         
-         // Update existing monsters/npc instance
-            monsterOrNpcToEdit?.setValue(game, forKey:  "gameName")
-            monsterOrNpcToEdit?.setValue(creatureName, forKey: "creatureName")
-            // monsterOrNpcToEdit?.setValue(date, forKey: "date") no date yet but can be added
-            monsterOrNpcToEdit?.setValue(maxHP, forKey: "hitPoints")
-            monsterOrNpcToEdit?.setValue(armor, forKey: "armorClass")
-            monsterOrNpcToEdit?.setValue(movement, forKey: "speed")
-            monsterOrNpcToEdit?.setValue(cR, forKey: "challengeRating")
-            monsterOrNpcToEdit?.setValue(xP, forKey: "xp")
-            monsterOrNpcToEdit?.setValue(str, forKey: "strength")
-            monsterOrNpcToEdit?.setValue(dex, forKey: "dexterity")
-            monsterOrNpcToEdit?.setValue(con, forKey: "constitution")
-            monsterOrNpcToEdit?.setValue(int, forKey: "intelligence")
-            monsterOrNpcToEdit?.setValue(wis, forKey: "wisdom")
-            monsterOrNpcToEdit?.setValue(cha, forKey: "charisma")
-            monsterOrNpcToEdit?.setValue(skillsProf, forKey: "skills")
-            monsterOrNpcToEdit?.setValue(radar, forKey: "senses")
-            monsterOrNpcToEdit?.setValue(speech, forKey: "languages")
-            monsterOrNpcToEdit?.setValue(abilities, forKey: "creatureFeatures")
-            monsterOrNpcToEdit?.setValue(attacks, forKey: "actions")
-            monsterOrNpcToEdit?.setValue(loot, forKey: "items")
+            // Check for all required fields
+            guard let game = gameName.text, !game.isEmpty else {
+                showAlert(forMissingField: "Game Name")
+                return
+            }
             
-            // npc vs monster boolean? add here
+            guard let creatureName = creatureNameText.text, !creatureName.isEmpty else {
+                showAlert(forMissingField: "Creature Name")
+                return
+            }
             
+            guard let maxHP = hitPoints.text, !maxHP.isEmpty else {
+                showAlert(forMissingField: "Hit Points")
+                return
+            }
             
+            guard let armor = armorClass.text, !armor.isEmpty else {
+                showAlert(forMissingField: "Armor Class")
+                return
+            }
             
-         } else {
-         // Create new NPC/Monster instance
-             let monsterOrNpc = NSEntityDescription.insertNewObject(forEntityName: "NPCorMonster", into: context)
-             monsterOrNpc.setValue(userID, forKey: "userID")
-             // note.setValue(date, forKey: "date") add date?
-             
-             monsterOrNpcToEdit?.setValue(game, forKey:  "gameName")
-             monsterOrNpcToEdit?.setValue(creatureName, forKey: "creatureName")
-             // monsterOrNpcToEdit?.setValue(date, forKey: "date") no date yet but can be added
-             monsterOrNpc.setValue(maxHP, forKey: "hitPoints")
-             monsterOrNpc.setValue(armor, forKey: "armorClass")
-             monsterOrNpc.setValue(movement, forKey: "speed")
-             monsterOrNpc.setValue(cR, forKey: "challengeRating")
-             monsterOrNpc.setValue(xP, forKey: "xp")
-             monsterOrNpc.setValue(str, forKey: "strength")
-             monsterOrNpc.setValue(dex, forKey: "dexterity")
-             monsterOrNpc.setValue(con, forKey: "constitution")
-             monsterOrNpc.setValue(int, forKey: "intelligence")
-             monsterOrNpc.setValue(wis, forKey: "wisdom")
-             monsterOrNpc.setValue(cha, forKey: "charisma")
-             monsterOrNpc.setValue(skillsProf, forKey: "skills")
-             monsterOrNpc.setValue(radar, forKey: "senses")
-             monsterOrNpc.setValue(speech, forKey: "languages")
-             monsterOrNpc.setValue(abilities, forKey: "creatureFeatures")
-             monsterOrNpc.setValue(attacks, forKey: "actions")
-             monsterOrNpc.setValue(loot, forKey: "items")
-             
-             // npc vs monster boolean? add here
+            guard let movement = speed.text, !movement.isEmpty else {
+                showAlert(forMissingField: "Speed")
+                return
+            }
+            
+            guard let cR = challengeRating.text, !cR.isEmpty else {
+                showAlert(forMissingField: "Challenge Rating")
+                return
+            }
+            
+            guard let xP = xpToEarn.text, !xP.isEmpty else {
+                showAlert(forMissingField: "XP to Earn")
+                return
+            }
+            
+            guard let str = strength.text, !str.isEmpty else {
+                showAlert(forMissingField: "Strength")
+                return
+            }
+            
+            guard let dex = dexterity.text, !dex.isEmpty else {
+                showAlert(forMissingField: "Dexterity")
+                return
+            }
+            
+            guard let con = constitution.text, !con.isEmpty else {
+                showAlert(forMissingField: "Constitution")
+                return
+            }
+            
+            guard let int = intelligence.text, !int.isEmpty else {
+                showAlert(forMissingField: "Intelligence")
+                return
+            }
+            
+            guard let wis = wisdom.text, !wis.isEmpty else {
+                showAlert(forMissingField: "Wisdom")
+                return
+            }
+            
+            guard let cha = charisma.text, !cha.isEmpty else {
+                showAlert(forMissingField: "Charisma")
+                return
+            }
+            
+            guard let skillsProf = skills.text, !skillsProf.isEmpty else {
+                showAlert(forMissingField: "Skills")
+                return
+            }
+            
+            guard let radar = senses.text, !radar.isEmpty else {
+                showAlert(forMissingField: "Senses")
+                return
+            }
+            
+            guard let speech = languages.text, !speech.isEmpty else {
+                showAlert(forMissingField: "Languages")
+                return
+            }
+            
+            guard let abilities = creatureFeatures.text, !abilities.isEmpty else {
+                showAlert(forMissingField: "Creature Features")
+                return
+            }
+            
+            guard let attacks = actions.text, !attacks.isEmpty else {
+                showAlert(forMissingField: "Actions")
+                return
+            }
+            
+            guard let loot = items.text, !loot.isEmpty else {
+                showAlert(forMissingField: "Items")
+                return
+            }
+            
+            // All required fields are present, proceed with entity creation
+            let userID = Auth.auth().currentUser?.uid
+            
+            if let monsterOrNpcToEdit = monsterOrNpcToEdit {
+                // Update existing monsters/npc instance
+                monsterOrNpcToEdit.setValue(game, forKey: "gameName")
+                monsterOrNpcToEdit.setValue(creatureName, forKey: "creatureName")
+                monsterOrNpcToEdit.setValue(maxHP, forKey: "hitPoints")
+                monsterOrNpcToEdit.setValue(armor, forKey: "armorClass")
+                monsterOrNpcToEdit.setValue(movement, forKey: "speed")
+                monsterOrNpcToEdit.setValue(cR, forKey: "challengeRating")
+                monsterOrNpcToEdit.setValue(xP, forKey: "xp")
+                monsterOrNpcToEdit.setValue(str, forKey: "strength")
+                monsterOrNpcToEdit.setValue(dex, forKey: "dexterity")
+                monsterOrNpcToEdit.setValue(con, forKey: "constitution")
+                monsterOrNpcToEdit.setValue(int, forKey: "intelligence")
+                monsterOrNpcToEdit.setValue(wis, forKey: "wisdom")
+                monsterOrNpcToEdit.setValue(cha, forKey: "charisma")
+                monsterOrNpcToEdit.setValue(skillsProf, forKey: "skills")
+                monsterOrNpcToEdit.setValue(radar, forKey: "senses")
+                monsterOrNpcToEdit.setValue(speech, forKey: "languages")
+                monsterOrNpcToEdit.setValue(abilities, forKey: "creatureFeatures")
+                monsterOrNpcToEdit.setValue(attacks, forKey: "actions")
+                monsterOrNpcToEdit.setValue(loot, forKey: "items")
+                // Add additional logic for NPC vs Monster if needed
+            } else {
+                // Create new NPC/Monster instance
+                let monsterOrNpc = NSEntityDescription.insertNewObject(forEntityName: "NPCorMonster", into: context)
+                monsterOrNpc.setValue(userID, forKey: "userID")
+                monsterOrNpc.setValue(game, forKey: "gameName")
+                monsterOrNpc.setValue(creatureName, forKey: "creatureName")
+                monsterOrNpc.setValue(maxHP, forKey: "hitPoints")
+                monsterOrNpc.setValue(armor, forKey: "armorClass")
+                monsterOrNpc.setValue(movement, forKey: "speed")
+                monsterOrNpc.setValue(cR, forKey: "challengeRating")
+                monsterOrNpc.setValue(xP, forKey: "xp")
+                monsterOrNpc.setValue(str, forKey: "strength")
+                monsterOrNpc.setValue(dex, forKey: "dexterity")
+                monsterOrNpc.setValue(con, forKey: "constitution")
+                monsterOrNpc.setValue(int, forKey: "intelligence")
+                monsterOrNpc.setValue(wis, forKey: "wisdom")
+                monsterOrNpc.setValue(cha, forKey: "charisma")
+                monsterOrNpc.setValue(skillsProf, forKey: "skills")
+                monsterOrNpc.setValue(radar, forKey: "senses")
+                monsterOrNpc.setValue(speech, forKey: "languages")
+                monsterOrNpc.setValue(abilities, forKey: "creatureFeatures")
+                monsterOrNpc.setValue(attacks, forKey: "actions")
+                monsterOrNpc.setValue(loot, forKey: "items")
+                // Add additional logic for NPC vs Monster if needed
+            }
+            
+            saveContext()
+            
+            if let monstersAndNpcVC = delegate as? RefreshMonsterTable {
+                monstersAndNpcVC.refreshMonsters()
+            }
+            
+            self.dismiss(animated: true) // need to adjust this so we don't get rid of the screen but refresh the save button
         }
-        
-        saveContext()
-        
-        let monstersAndNpcVC = delegate as! RefreshMonsterTable
-        monstersAndNpcVC.refreshMonsters()
-        
-        self.dismiss(animated: true)
-        }
-    }
+
 
     func saveContext () {
         if context.hasChanges {
